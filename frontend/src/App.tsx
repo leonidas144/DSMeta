2ecd614e94a713389de231c472575ebf0ff84723
leonidas144 <leo_alvarenga92@hotmail.com>
2022-09-06 13:37:28 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
-  return(<h1>Ola mundo</h1>
+  return(
+    <>
+    <h1>Ola mundo</h1>
+    <NotificationButton />
+    </>
 
   )
 }
