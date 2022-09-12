38123c7901c8c5df9cc6bc5f8fb60e33dc621cfe
leonidas144 <leo_alvarenga92@hotmail.com>
2022-09-11 22:16:51 -0300

    Axios, useffect first request

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -3,7 +3,8 @@ import './styles.css'
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function SalesCard() {
 
@@ -13,6 +14,14 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+    useEffect(()=>{
+        axios.get("https://dsmeta-leonidas.herokuapp.com/sales")
+            .then(response => {
+                console.log(response.data);
+
+            });
+
+    },[]);
 
     return (
         <>
